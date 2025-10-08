bc1bf0ba1b3a45ad5a57a2c4e7b733252ad7640a
Sebeaty <102726372+Sebeaty@users.noreply.github.com>
2025-10-08 16:44:16 +0200

    Create Q6.java

---
+++ Q6.java
@@ -0,0 +1,17 @@
+public class Q6
+{
+  public String triangle(int n)
+  {
+    String s = "";
+    for(int i =n;i>=0;i--)
+      {
+        for(int j=n;j>=i;j--)
+        {
+          s+="x ";
+        }
+        s+="\n";
+      }
+    System.out.println(s);
+    return s
+  }
+}
