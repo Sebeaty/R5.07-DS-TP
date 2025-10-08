6ab5b405d185c2f2e6bb43422adea2916bca5999
Sebeaty <102726372+Sebeaty@users.noreply.github.com>
2025-10-08 16:42:25 +0200

    Update Q5.java
    changer l'affichage du triangle

--- Q5.java
+++ Q5.java
@@ -3,9 +3,9 @@ public class Q5
   public String triangle(int n)
   {
     String s = "";
-    for(int i =0;i<=n;i++)
+    for(int i =n;i>=0;i--)
       {
-        for(int j=0;j<=i;j++)
+        for(int j=n;j>=i;j--)
         {
           s+=""+j+" ";
         }
